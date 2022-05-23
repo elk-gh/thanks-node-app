b501b2ffb207151567e80a8771964720ccb2e8eb
Elkin Cordoba <elkin.cordoba@cloudgaia.com>
2022-05-22 23:54:18 -0500

    issue fix

--- apps/ready-to-fly/user-interface/modals/index.js
+++ apps/ready-to-fly/user-interface/modals/index.js
@@ -1,9 +1,6 @@
 'use strict';
 const { authorizeSalesforcePrompt } = require('./authorize-sf-prompt');
 const { createRecognitionForm } = require('./create-recognition-form');
-const {
-    travelRequestCreationFailed
-} = require('./travel-request-create-failed');
 
 module.exports = {
     authorizeSalesforcePrompt,

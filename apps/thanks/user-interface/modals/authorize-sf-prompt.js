169fc6a5bc7c73540626a82e47efd86a988035f6
Elkin Cordoba <elkin.cordoba@cloudgaia.com>
2022-05-24 21:55:39 -0500

    shortcut update

--- apps/thanks/user-interface/modals/authorize-sf-prompt.js
+++ apps/thanks/user-interface/modals/authorize-sf-prompt.js
@@ -2,7 +2,7 @@
 const { Modal, Blocks } = require('slack-block-builder');
 
 const authorizeSalesforcePrompt = (teamId, appId) => {
-    return Modal({ title: 'Ready to Fly', close: 'Close' })
+    return Modal({ title: 'Thanks!', close: 'Close' })
         .blocks(
             Blocks.Section({
                 text: `*This shortcut requires to link Slack to your Salesforce account.*\n\n Navigate to <slack://app?team=${teamId}&id=${appId}&tab=home|App Home> and click *Authorize with Salesforce* to connect your Salesforce account`

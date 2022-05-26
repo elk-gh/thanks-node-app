e91e679557ffed1a762d0681d88a4e547158c540
Elkin Cordoba <elkin.cordoba@cloudgaia.com>
2022-05-26 00:46:48 -0500

    registered modals

--- apps/thanks/user-interface/modals/index.js
+++ apps/thanks/user-interface/modals/index.js
@@ -1,8 +1,16 @@
 'use strict';
 const { authorizeSalesforcePrompt } = require('./authorize-sf-prompt');
 const { createRecognitionForm } = require('./create-recognition-form');
+const {
+    createRecognitionRequestSuccess
+} = require('./create-recognition-success');
+const {
+    createRecognitionRequestCreationFailed
+} = require('./create-recognition-failed');
 
 module.exports = {
     authorizeSalesforcePrompt,
-    createRecognitionForm
+    createRecognitionForm,
+    createRecognitionRequestSuccess,
+    createRecognitionRequestCreationFailed
 };
